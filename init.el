18fbb1cc49df2089249bf9f9e3a917dc21fb5c4f
Winkey <winkeyme@163.com>
2017-10-30 21:50:02 +0800

    Setting chinese font

--- init.el
+++ init.el
@@ -477,7 +477,18 @@ It should only modify the values of Spacemacs settings."
 
   ;; https://emacs-china.org/t/topic/186/35
   (setq redisplay-dont-pause nil)
-  )
+
+  ;; setting chinese font
+  (when window-system
+    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+      (set-fontset-font (frame-parameter nil 'font)
+                        charset
+                        (or
+                         (and (spacemacs/system-is-mac) (font-spec :family "Hiragino Sans GB" :size 16))
+                         (and (spacemacs/system-is-linux) (font-spec :family "WenQuanYi Micro Hei" :size 16))
+                         (and (spacemacs/system-is-mswindows (font-spec :family "Microsoft Yahei" :size 14))))
+                        )))
+    )
 
 (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
 (load custom-file 'no-error 'no-message)
