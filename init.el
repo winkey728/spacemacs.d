6f05f26f54b06d0309d6e516d001d66014feb065
winkey <winkeyme@163.com>
2018-04-06 15:58:29 +0800

    Customize indentation config

--- init.el
+++ init.el
@@ -558,6 +558,18 @@ It should only modify the values of Spacemacs settings."
         "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
 
   (setq exec-path-from-shell-check-startup-files nil)
+
+  ;; Setup my indentation for different mode
+  (progn
+    (setq c-basic-offset 4)             ; c/c++/java...
+    (setq javascript-indent-level 2)    ; javascript-mode
+    (setq js-indent-level 2)            ; js-mode
+    (setq js2-basic-offset 2) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+    (setq web-mode-markup-indent-offset 2) ; web-mode, html tag in html file
+    (setq web-mode-css-indent-offset 2)    ; web-mode, css in html file
+    (setq web-mode-code-indent-offset 2)   ; web-mode, js code in html file
+    (setq css-indent-offset 2)             ; css-mode
+    )
   )
 
 (defun dotspacemacs/user-config ()
